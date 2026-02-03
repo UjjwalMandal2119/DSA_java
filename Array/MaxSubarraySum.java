27b4106f0a0586b1d79bab7added189b32213a30
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-03 16:58:20 +0530

    Documentation

--- Array/MaxSubarraySum.java
+++ Array/MaxSubarraySum.java
@@ -3,12 +3,13 @@ public class MaxSubarraySum {
         int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
         System.out.println(MaxSum(arr));
     }
-
+//two loop using to calulate the sum and returning the maximum value;
+//one loop for the first index and all subArray started from that index
     public static int MaxSum(int [] arr){
         int n=arr.length;
         int ans=Integer.MIN_VALUE;
         for(int i=0;i<n; i++){
-            int sum=0;
+            int sum=0;      //sum must be zero after one subarray
             for(int j=i; j<n; j++){
                 sum+=arr[j];
                 ans=Math.max(ans,sum);
