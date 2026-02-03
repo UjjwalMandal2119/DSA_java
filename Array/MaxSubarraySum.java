8dcfa3338ef2faa8334e1d8e20f457a941820d9a
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-03 16:55:44 +0530

    learning

---
+++ Array/MaxSubarraySum.java
@@ -0,0 +1,19 @@
+public class MaxSubarraySum {
+    public static void main(String args[]){
+        int [] arr = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(MaxSum(arr));
+    }
+
+    public static int MaxSum(int [] arr){
+        int n=arr.length;
+        int ans=Integer.MIN_VALUE;
+        for(int i=0;i<n; i++){
+            int sum=0;
+            for(int j=i; j<n; j++){
+                sum+=arr[j];
+                ans=Math.max(ans,sum);
+            }
+        }
+        return ans;
+    }
+}
