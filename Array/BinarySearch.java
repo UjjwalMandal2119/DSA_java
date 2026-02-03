0c7b3586732e8200b9caabec84ce5ec0ae696b85
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-03 08:31:28 +0530

    Binary  Search Documentation

--- Array/BinarySearch.java
+++ Array/BinarySearch.java
@@ -1,3 +1,14 @@
+//for binary search we need sorted data
+//Divide and Conquare , defining lower and higher index and Calulate the mid, 
+//low index=0; and high index =arr.length-1;
+//Mid is the main thing in Binary Search ,We compare mid with the target value 
+//if it found we return the mid index
+//if value grater than target we assign high point = mid-1;
+//else low point = mid+1;
+// if elements not found we return -1;
+// we use while loop beacuse we have no idea how many iteration going on.
 public class BinarySearch {
-    
+    public static void main(String args[]){
+        int [] arr= {1,2,4,6,7,89,90,100,101};
+    }
 }
