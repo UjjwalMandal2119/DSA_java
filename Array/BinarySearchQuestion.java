6b18d2231a4b7940e3f12a876d12b530b298f4f9
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-03 09:06:34 +0530

    Documentation

--- Array/BinarySearchQuestion.java
+++ Array/BinarySearchQuestion.java
@@ -1,3 +1,12 @@
+//Documentation
+//A question given that we have to satisfy the condition with max possible value
+//condition is (x^k<=n) here user give us k and n we have to return the max possible value of x
+//Linaer search is good for lower value but it doesn't fit for a large number of n
+//We perform Binary Search for Optimization the solution 
+//It good for time complexity and memory Optimization
 public class BinarySearchQuestion {
+    public static void main(String args[]){
+        
+    }
     
 }
