85d04ba24d5fbcd6db10dceba2f9ab8b19efedef
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-06 07:18:35 +0530

    learning 2D array

---
+++ TwoD_Array/TwoDArray.java
@@ -0,0 +1,12 @@
+package TwoD_Array;
+
+public class TwoDArray {
+    public static void main(String args[]){
+        int [][] arr= new int [3][4];
+        System.out.println(arr);
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[0][1]);
+   }
+}
