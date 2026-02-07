c8bdd466f0cf91de79cd370829d2bf891446f519
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-07 07:52:37 +0530

    learning by doing

--- TwoD_Array/twoDArray2.java
+++ TwoD_Array/twoDArray2.java
@@ -2,7 +2,9 @@ package TwoD_Array;
 
 public class twoDArray2 {
     public static void main(String args[]){
-        int [] [] arr={{1,2,3},{4,5,6},{7,8,9}};
+        int [] [] arr={{1,2,3},
+                       {4,5,6},
+                       {7,8,9}};
         Display(arr);
     }
     public static void Display(int [][] arr){
