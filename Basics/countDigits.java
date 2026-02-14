e58fefe74d5673e5ffeaf503efd84e7b72573187
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-14 18:41:59 +0530

    learning

---
+++ Basics/countDigits.java
@@ -0,0 +1,22 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class countDigits {
+    public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the number");
+         int n= sc.nextInt();
+         System.out.println("Total number of digits " + CountDigOfNumber(n));
+
+    }
+    //method for counting the digits
+    public static int CountDigOfNumber(int n){
+        int count =0;
+     while(n>0){
+        n=n/10;
+        count++;
+     }
+     return count;
+    }
+}
