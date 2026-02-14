8ea325a5c1effbb55a5fa7709039475291bb230f
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-14 20:04:21 +0530

    learning

---
+++ Basics/Facctorial.java
@@ -0,0 +1,27 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Facctorial {
+        public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the number");
+         int n= sc.nextInt();    
+         Facctorial1(n);
+         //Facctorial2(n);
+        }
+          
+        //Iteration Method
+        public static void Facctorial1(int n){
+            //early exit case
+            if(n==0 ||n==1) 
+                System.out.println(1);
+            int fac=1;
+            for(int i=1; i<=n;i++){
+             fac= fac*i;
+             System.out.print(i +" ");
+            }
+            System.out.println();
+            System.out.println("Factorial of given number " + fac);
+        }
+}
