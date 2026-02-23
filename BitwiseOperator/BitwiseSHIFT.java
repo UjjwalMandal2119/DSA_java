daa253cf2852ca3c32652bd716e813cc7245f1e4
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-23 20:51:17 +0530

    learning

--- BitwiseOperator/BitwiseSHIFT.java
+++ BitwiseOperator/BitwiseSHIFT.java
@@ -7,21 +7,27 @@ public class BitwiseSHIFT {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the first number");
         int n1=sc.nextInt();
-        System.out.println("enter the second number");
-        int n2 =sc.nextInt();
+       
         
         int c1 =n1 << 1; //leftshift operator  the direction of arrow indicate toward the left side
+        //from left shift operator we cn say that the value will increase
 
-        int c2 =n2 >> 1; //rightshift operator the dirction of arrow indicate toward the right side
+        int c2 =n1 >> 1; //rightshift operator the dirction of arrow indicate toward the right side
+        //with right side operator value get decrase as we can say that the LSB get VANISHED;
+
+        int c3 =n1 >>> 1;  //unsigned right shift
        
        System.out.println(Integer.toBinaryString(n1));
-       System.out.println(Integer.toBinaryString(n2));
+    
        System.out.println("left shift");
        System.out.println(Integer.toBinaryString(c1));
        System.out.println(c1);
        System.out.println("right shift");
        System.out.println(Integer.toBinaryString(c2));
        System.out.println(c2);
+       System.out.println("unsigned rigth shift");
+       System.out.println(Integer.toBinaryString(c3));
+       System.out.println(c3);
     
     }
  
