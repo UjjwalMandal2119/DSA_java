dd0585b381b3009d70240dd748d65426f6f6e07a
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-24 10:57:10 +0530

    learning

--- String/BasicString.java
+++ String/BasicString.java
@@ -6,18 +6,21 @@ public class BasicString {
         String str2= new String("Hello");
          String str3="Hello";
         String str4= new String("Hello");
-        System.out.println(str1);
+        System.out.println(str1);             //here content is same but memory location is different
         System.out.println(str2);
         System.out.println(str3);
         System.out.println(str4);
+        System.out.println(str1.length());
     }
 }
+//we cannot print address in String 
+//because string class ke ander object class ke  toString() method ko override kar rakha hai so it print contents
 
 
 //string non -primitive data type ,HEAP Memory for store contents
 //address in STACk memory
 
-//hardcore ,string allocated memory in string pool/Internal pool , a special area in string memory 
+//hardcode ,string allocated memory in string pool/Internal pool , a special area in string memory 
 //in this area duplicate string not allowed 
 //if in string pool for duplicate string the same address allocated 
 
