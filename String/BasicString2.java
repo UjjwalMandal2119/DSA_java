157f11591bfd92997d4c1a139bab88eda37d7293
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-24 15:21:59 +0530

    learning

--- String/BasicString2.java
+++ String/BasicString2.java
@@ -3,6 +3,8 @@ package String;
 public class BasicString2 {
     public static void main(String[] args) {
         String s ="hello";
-        System.err.println(10 + s +30 + 40);
+        System.out.println(10 + s +30 + 40); //string plus something always string o/p 10hello3040  string  + number =string=number +string;
+        System.out.println(10 + 20+ 30 +s +10); //here 60hello10  ,first number + number =number 
+        System.out.println(10 +s +(30 +40)); //operator precceding
     }
 }
