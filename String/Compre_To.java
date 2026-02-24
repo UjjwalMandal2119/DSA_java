d3526b17e6f9eda57232dae7881e1d39d0cd8489
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-24 19:25:04 +0530

    learning

--- String/Compre_To.java
+++ String/Compre_To.java
@@ -3,7 +3,7 @@ package String;
 public class Compre_To {
     public static void main(String[] args) {
         String s1 = "Komal";
-        String s2 = "Kamal";
+        String s2 = "Komalaa";
         //System.out.println(s1.compareTo(s2)); //if 0 strings are equal, positive S1 greater , negative s2 GREATER
 
         //calling a method in main method
@@ -12,7 +12,7 @@ public class Compre_To {
     public static int compareTo(String s1, String s2){
         for(int i=0; i<Math.min(s1.length(),s2.length()); i++){
             if(s1.charAt(i) != s2.charAt(i)){
-                return s1.charAt(i) -s2.charAt(i);
+                return s1.charAt(i) -s2.charAt(i);  //>0 =>s1> s2 ||<0 =>s1<s2;  comparing with ASCII Value;
             }
         }
         return s1.length() - s2.length();
