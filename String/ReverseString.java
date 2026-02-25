3f35dc812ce1fd9898bf183d880e0f2c575c3226
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-25 17:07:53 +0530

    learning

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -7,25 +7,33 @@ public class ReverseString {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the string");
         String s = sc.next();
-        //System.out.println(RevString(s));
+        System.out.println(RevString(s));
         System.out.println("reverse string");
-      RevString1(s);
+        RevString1(s);
+        System.out.println(RevString2(s));
     }
+    //using a array
+    //first I convert thid string into an array of character
+    //then apply two pointer approach ,beacuse string is immutable in java
+    //last we convert char array into string.
     public static String RevString(String s){
-        if(s.length()==0 || s.length()==1) return s;
-
-        int l=0;
-        int h= s.length()-1;
-        while(l<h){
-            String temp =s.charAt(l);
-            s.charAt(l) =s.charAt(h);
-            s.charAt(h) = temp;
-            l++;
-            h--;
+        if(s.length()==0 || s.length()==1) {
+        return s;
         }
- return s;
+       char [] arr=s.toCharArray();
+       int l=0;
+       int h=arr.length -1;
+       while(l<h){
+        char temp =arr[l];
+        arr[l]=arr[h];
+        arr[h]=temp;
+        l++;
+        h--;
+       } 
+       return new String(arr);
 
     }
+    //using a extra null string and adding one by one
     public static void RevString1(String s){
      if(s.length()==0 || s.length()==1) {
         System.out.println(s);
@@ -36,4 +44,8 @@ public class ReverseString {
      }
      System.out.println(rev);
     }
+    //java inbuild function
+     public static String RevString2(String s){
+         return new StringBuilder(s).reverse().toString();
+     }
 }
