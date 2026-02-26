97932899dfd3fd643c5f8068a0da2e3e81af37b5
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-26 09:54:00 +0530

    learning

--- Array/SortIntegersby1sBit.java
+++ Array/SortIntegersby1sBit.java
@@ -25,9 +25,12 @@ public class SortIntegersby1sBit {
        List<int []> list =new ArrayList<>();
 
        for(int a :arr){
-        int count = Integer.bitCount(a);
+        //this counts numbers of 1's
+        //int count = Integer.bitCount(a);
+        int count =countSetBits(a);
         list.add(new int[]{a, count});
        }
+       //Comparator function
        Collections.sort(list, (a,b) -> a[1] != b[1] ? a[1] -b[1] : a[0] - b[0]);
        int [] res = new int[n];
        for(int i=0; i<list.size(); i++){
@@ -35,4 +38,27 @@ public class SortIntegersby1sBit {
        }
        return res;
     }
+
+    //Brian kernighan's Algorithm
+    public static int countSetBits(int n){
+        int count=0;
+        while(n>0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+
+    public static int counts(int n){
+        int count=0;
+        while(n>0){
+           count+=n&1;
+           n=n>>1;
+            }
+        return count;
+    }
+
 }
+// Integer.bitCount(a) returns the number of set bits (1s) in the binary representation of the integer a.
+
+// This is also called the Hamming weight or population count.
