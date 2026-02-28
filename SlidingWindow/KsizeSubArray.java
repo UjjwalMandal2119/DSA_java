8f798722ff1a59b7fce43d775b47d2590387299e
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-28 13:18:09 +0530

    learning Sliding Window

---
+++ SlidingWindow/KsizeSubArray.java
@@ -0,0 +1,25 @@
+package SlidingWindow;
+
+public class KsizeSubArray {
+    public static void main(String[] args) {
+        int k =3;
+        int [] arr  ={ 1,3,4,5,6,7,8,9,43};
+        System.out.println(MaxSumofSizeK(arr, k));
+    }
+    public static int MaxSumofSizeK(int [] arr, int k){
+  //Pahele first window ka Kaam
+     int sum=0;
+     int ans=0;
+     for(int i=0; i<k; i++){
+        sum+=arr[i];
+
+     }
+     ans= sum;
+     for(int i=k; i<arr.length; i++){
+        sum+=arr[i]; //size grow by one
+        sum -= arr[i-k]; //pichhe se ek kam kar diye
+        ans=Math.max(ans, sum);
+     }
+     return ans;
+    }
+}
