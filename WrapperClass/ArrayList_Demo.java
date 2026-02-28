cb7d8ed8efd631d5a1ded42c249af5015a17682b
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-28 09:33:14 +0530

    leaning

---
+++ WrapperClass/ArrayList_Demo.java
@@ -0,0 +1,13 @@
+package WrapperClass;
+
+
+import java.util.ArrayList;
+
+public class ArrayList_Demo { //ArrayList ek class h ,size ka limitation nhi hota
+    public static void main(String[] args) {
+        ArrayList<Integer> list =new ArrayList<>();
+        System.out.println(list); //content print hota h
+        System.out.println(list.size());
+
+    }
+}
