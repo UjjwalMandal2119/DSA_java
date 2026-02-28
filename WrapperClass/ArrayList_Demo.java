76d24f0cec51ed06c090eedb6d9dc1d934ac454d
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-02-28 09:39:50 +0530

    learning

--- WrapperClass/ArrayList_Demo.java
+++ WrapperClass/ArrayList_Demo.java
@@ -9,5 +9,19 @@ public class ArrayList_Demo { //ArrayList ek class h ,size ka limitation nhi hot
         System.out.println(list); //content print hota h
         System.out.println(list.size());
 
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list); //O/P:[10, 20, 30]  add pichhe pichhe add kar deta h
+        System.out.println(list.size());
+
+        list.add(40);
+        list.add(50);
+        // insertion in between
+        //concept of indexing aur hame index ka range pta karna hoga
+        list.add(2, -6); //put the index where we have to insert the value
+        System.out.println(list); 
+
     }
 }
