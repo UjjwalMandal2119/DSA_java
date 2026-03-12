23f89dcc02296d76fa17ff8d09f3f8f7b27ceb02
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-03-12 22:49:45 +0530

    learning

---
+++ Array/BooleanDataType.java
@@ -0,0 +1,8 @@
+public class BooleanDataType {
+    public static void main(String arg[]){
+        int a=10; b=10; c=10;
+        boolean z;
+        z=(a==b==c);
+        System.out.println(z);
+    }
+}
