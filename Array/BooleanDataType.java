14754e0a39ed01a7df5adcd0081b22f4553eeea7
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-03-12 23:00:30 +0530

    learnin

--- Array/BooleanDataType.java
+++ Array/BooleanDataType.java
@@ -1,7 +1,8 @@
 public class BooleanDataType {
     public static void main(String arg[]){
-        int a=10; b=10; c=10;
+        int a=10, b=10, c=10;
         boolean z;
+        //a==b true but c=10 integer type so cannot be resolved to a variable 
         z=(a==b==c);
         System.out.println(z);
     }
