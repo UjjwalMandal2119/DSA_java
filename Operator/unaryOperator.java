b465d0a91e1d85a40cb11a1c01ac01aaaea28451
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-03-13 18:57:10 +0530

    learning

--- Operator/unaryOperator.java
+++ Operator/unaryOperator.java
@@ -2,6 +2,15 @@ package Operator;
 
 public class unaryOperator {
     public static void main(String[] args) {
-        
+        byte a=50, b=20;
+        System.out.println(++a>20 || ++b>2); //true;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
+//boolean logical & and  | 
+
+//logical Operator
+
+//&& || ! 
+//&& and || also shortcircuit Operator
