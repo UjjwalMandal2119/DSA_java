00e5565a414c555de7f7814baa5b8bfed0e76a11
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-03-13 19:59:42 +0530

    learning

--- Operator/Loop/loop.java
+++ Operator/Loop/loop.java
@@ -12,8 +12,9 @@ public class loop {
         for(int j=0; j<=10; j++){ //we cann also declare the variable in for loop , block scope
             System.out.println(j);
         }
-        for(i=1; i<=5; i++);
+        for(i=1; i<=5; i++); //blank statement  
         System.out.println(i);
-
+         for(i =5; i<10; i++)  //bydefault ek statement ko read karega har ek loop
+            System.out.println(i);
     }
 }
