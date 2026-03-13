e12d8bfb0d0af698ef702b501854c94cfc0af74e
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-03-13 19:48:33 +0530

    learninig

---
+++ Operator/Loop/loop.java
@@ -0,0 +1,13 @@
+package Operator.Loop;
+
+public class loop {
+    public static void main(String arg[]){
+        int i;
+        i=1; //initilization
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
