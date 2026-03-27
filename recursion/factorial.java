a0739b4b3d411dacce32a6c5fd8ddabd0353f65a
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-03-28 00:27:39 +0530

    learning

--- recursion/factorial.java
+++ recursion/factorial.java
@@ -1,7 +1,18 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class factorial {
     public static void main (String args[]){
-        
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+   System.out.println(fact(n));
+    }
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fn = fact(n-1);
+        return n*fn;
     }
 }
