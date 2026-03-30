1f8ab8621c7d48604284ee4f82831f9a683eec0c
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-03-31 01:07:52 +0530

    learning

--- recursion/LetterCombinationofaPhoneNumber17.java
+++ recursion/LetterCombinationofaPhoneNumber17.java
@@ -1,5 +1,26 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LetterCombinationofaPhoneNumber17 {
-     
+     static String[] code ={"", "", "abc", "def", "ghi", "jkl" ,"mno", "pqrs", "tuv" , "wxyz"};
+     public static void main(String[] args) {
+         String ques = "23";
+         List<String> ll = new ArrayList<>();
+         PrintAnswer(ques, "",ll);
+         System.out.println(ll);
+     }  
+     public static void PrintAnswer(String ques, String ans, List<String> ll){
+        if(ques.length()==0){
+            //System.out.print(ans + " ");
+            ll.add(ans);
+            return;
+        }
+        char ch = ques.charAt(0);//2
+        String press =code[ch -48]; //abc
+        for(int i=0; i<press.length(); i++){
+            PrintAnswer(ques.substring(1), ans + press.charAt(i), ll);
+        }
+     }
 }
