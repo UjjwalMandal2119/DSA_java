74ed3dd6507b2f250c60832e28b84c7e53ea781f
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-02 10:53:14 +0530

    learning time and space complexity

---
+++ Time_Space_Complexity/Demo_2.java
@@ -0,0 +1,11 @@
+package Time_Space_Complexity;
+
+public class Demo_2 {
+    public static void main(String args[]){
+        System.out.println("Always work with Diligently");
+        System.out.println("Hard Work always rocks");
+    }
+}
+
+//Time complexity O(1) constant
+//Arithmetic operation are in constant Time Complexity
---
+++ Time_Space_Complexity/Demo_3.java
@@ -0,0 +1,19 @@
+package Time_Space_Complexity;
+//linear Search
+
+public class Demo_3 {
+    public static void main(String[] args) {
+        int [] arr={1,3,65,7,4,7,4,88,15};
+        int target =15;
+        System.out.println();
+        System.out.println(Search(arr, target));
+    }
+    public static int Search(int [] arr , int target){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
