dd5a55196f23482e2c5bdd69299bf1587fbcdb65
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-04 23:28:06 +0530

    leetcode

---
+++ Stack_and_Queue/LC277_Find_celebrity.java
@@ -0,0 +1,38 @@
+package Stack_and_Queue;
+
+import java.util.Stack;
+
+public class LC277_Find_celebrity {
+    public static void main(String[] args) {
+        int [][] arr ={{0,1,1,1},{1,0,1,1},{0,0,0,0},{1,1,1,0}};
+        System.out.println(Celebrity(arr));
+    }
+    public static int Celebrity(int [][] arr) {
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<arr.length; i++){
+            st.push(i);
+        }
+        //Elimination
+
+        while(st.size() >1){
+            int a=st.pop();
+            int b= st.pop();
+            if(arr[a][b]==1){
+                st.push(b);
+
+            }else{
+                st.push(a);
+            }
+        }
+        int cand = st.pop();
+        for(int i=0; i<arr.length; i++){
+            if(i == cand){
+                continue;
+            }
+           if(arr[cand][i] !=0 || arr[i][cand]==0 ){
+            return -1;
+           } 
+        }
+        return cand;
+    }
+}
