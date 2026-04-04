645dfc1aa7cf667ba574d4a11b5c0c0262038873
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-05 04:19:35 +0530

    learning linkedlist

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -8,4 +8,140 @@ public class LinkedList1 {
     private Node head;  //1st Node Address
     private int size; //number of Node
     private Node tail; //last Node of LinkedList
+
+
+    //TC O(1)
+    public void addfirst(int item){
+        Node nn =new Node();
+        nn.val=item;
+        if(size ==0){
+            head=nn;
+            tail=nn;
+            size++;
+        }else{
+            nn.next=head;
+            head =nn;
+            size++;
+        }
+
+    }
+     //O(1)
+    public void addlast(int item){
+    if(size==0){
+        addfirst(item);
+      }else{
+        Node nn= new Node();
+        nn.val = item;
+        tail.next =nn;
+        tail =nn;
+        size++;
+
+      }
+    }
+ 
+    //O(n)
+    public void addatindex(int item, int k) throws Exception{
+        if(k==0){
+            addfirst(item);
+        }else if(k==size){
+            addlast(item);
+        }else{
+            Node k_1th = GetNode(k-1);
+            Node nn= new Node();
+            nn.val=item;
+            nn.next=k_1th.next;
+            k_1th.next =nn;
+            size++;
+        }
+        
+    }
+
+    private Node GetNode(int k) throws Exception{
+        if(k<0 ||k>=size){
+            throw new Exception("K out of range");
+        }
+        Node temp = head;
+        for(int i=0; i< k; i++){
+            temp= temp.next;
+        }
+        return temp;
+    }
+
+
+    public void Display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.val + "->");
+            temp=temp.next;
+        }
+        System.out.println(".");
+    }
+
+    public int getFirst() throws Exception{
+
+        if(head == null){
+            throw new Exception("LinkedList is Empty");
+        }
+      return head.val;
+    }
+//O(1)
+    public int getLast()throws Exception{
+        if(head == null){
+            throw new Exception("LinkedList is Empty");
+        }
+        return tail.val;
+    }
+//O(n)
+    public int getatIndex(int k)throws Exception{
+          return GetNode(k).val;
+    }
+
+//O(1)
+
+    public int removeFirst(){
+        Node temp = head;
+        if(size ==1){
+            head = null;
+            tail = null;
+
+        }else{
+            head =head.next;
+            temp.next=null;
+        }
+        size--;
+        return temp.val;
+    }
+
+    public int removeLast() throws Exception{
+        if(size ==1){
+            return removeFirst();
+        }else{
+            Node prev =GetNode(size -2);
+            Node temp =prev;
+            tail=prev;
+            tail.next=null;
+            size--;
+            return temp.val;
+        }
+         
+    }
+
+    public int removeatIndex(int k) throws Exception{
+        if(k==0){
+            return removeFirst();
+        }else if(k==size-1){
+            return removeLast();
+        }else{
+            Node prev =GetNode(k-1);
+            Node curr = prev.next; //getatIndex(k)
+            prev.next =curr.next;
+            curr.next=null;
+            size--;
+            return  curr.val;
+
+        }
+        
+
+    }
+
 }
--- LinkedList/LinkedList1_Client.java
+++ LinkedList/LinkedList1_Client.java
@@ -1,7 +1,23 @@
 package LinkedList;
 
 public class LinkedList1_Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         LinkedList1 l = new LinkedList1();
+        l.addfirst(10);
+        l.addfirst(20);
+        l.addfirst(30);
+        l.Display();
+        l.addlast(-9);
+        l.addatindex(89, 2);
+        l.Display();
+        System.out.println(l.getatIndex(3));
+        System.out.println(l.getFirst());
+        System.out.println(l.getLast());
+        System.out.println(l.removeFirst());
+        l.Display();
+        System.out.println(l.removeLast());
+        l.Display();
+        System.out.println(l.removeatIndex(2));
+        l.Display();
     }
 }
