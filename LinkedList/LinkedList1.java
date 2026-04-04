957ec1137fc3aa4d67ed419fe601a993b5ec739d
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-05 00:18:15 +0530

    learning linkedlist

---
+++ LinkedList/LinkedList1.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class LinkedList1 {
+    public class Node{
+        int val;
+        Node next;
+    }
+    private Node head;  //1st Node Address
+    private int size; //number of Node
+    private Node tail; //last Node of LinkedList
+}
---
+++ LinkedList/LinkedList1_Client.java
@@ -0,0 +1,7 @@
+package LinkedList;
+
+public class LinkedList1_Client {
+    public static void main(String[] args) {
+        LinkedList1 l = new LinkedList1();
+    }
+}
