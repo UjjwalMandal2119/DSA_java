da37c882dc04d193603b3663fb1797ff1a53c12e
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-07 01:57:33 +0530

    learning Balanced Binary Tree

---
+++ Tree/Diameter_of_BinaryTree_Optimized.java
@@ -0,0 +1,38 @@
+package Tree;
+
+public class Diameter_of_BinaryTree_Optimized {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+  class Solution{
+    public int diameterOfBinaryTree(TreeNode root){
+           return diameter(root).dt;
+    }
+    public DiaPair diameter(TreeNode root){
+        if(root == null){
+            return new DiaPair();
+        }
+        DiaPair ldp = diameter(root.left); //left diameter pair =ldp (d, h)
+        DiaPair rdp = diameter(root.right); // right diameter pair = rdp  (d,h)
+        DiaPair sdp = new DiaPair();
+        int sd = ldp.ht + rdp.ht +2;
+        sdp.dt = Math.max(sd, Math.max(ldp.dt, rdp.dt));
+        sdp.ht = Math.max(ldp.ht, rdp.ht) +1;
+        return sdp;
+    }
+    class DiaPair{
+        int dt=0;
+        int  ht =-1;
+    }
+  }
+}
--- Tree/LC543_Diameter_of_Binary_Tree.java
+++ Tree/LC543_Diameter_of_Binary_Tree.java
@@ -16,7 +16,22 @@ public class LC543_Diameter_of_Binary_Tree {
   }
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        
+        if(root == null){
+            return 0;
+        }
+        int ld = diameterOfBinaryTree(root.left);
+        int rd = diameterOfBinaryTree(root.right);
+        int sd = ht(root.left) + ht(root.right) +2;
+        return Math.max(rd, Math.max(ld, sd));
+    }
+
+    public int ht(TreeNode root){
+        if(root == null){
+            return -1;
+        }
+        int lh= ht(root.left);
+        int rh = ht(root.right);
+        return Math.max(lh, rh) +1;
     }
 }
 
