0abdbac8a572501e53e2b9a8284049cac7f55c89
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-07 04:15:58 +0530

    ...

---
+++ BinarySearch_Tree/LC450_Delete_Node_in_a_BST.java
@@ -0,0 +1,54 @@
+package BinarySearch_Tree;
+
+public class LC450_Delete_Node_in_a_BST {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+  class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+         return  delete(root, key);
+    }
+    public TreeNode delete(TreeNode root, int key){
+
+        if(root == null){
+            return null;
+        }
+   if(root.val <key){
+    root.right =delete(root.right, key);
+   }else if(root.val >key){
+    root.left = delete(root.left, key);
+   }else{
+    //0 and 1 child
+     if(root.left == null){
+        return root.right;
+     }else if(root.right == null){
+        return root.left;
+     }else{
+       int max =max(root.left);
+       root.left =delete(root.left, max);
+       root.val =max;
+
+     }
+   }
+    return root;
+    }
+    private int max(TreeNode node){
+        if(node == null){
+            return Integer.MIN_VALUE;
+        }
+
+        int right=max(node.right);
+        return Math.max(node.val, right);
+    }
+}
+}
