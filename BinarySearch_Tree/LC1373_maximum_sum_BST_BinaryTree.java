93477f7835cc3bdca812e03c9fb63077a88b494d
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-07 13:24:43 +0530

    learning BST

--- BinarySearch_Tree/LC1373_maximum_sum_BST_BinaryTree.java
+++ BinarySearch_Tree/LC1373_maximum_sum_BST_BinaryTree.java
@@ -18,7 +18,35 @@ public class LC1373_maximum_sum_BST_BinaryTree {
 
     class Solution {
     public int maxSumBST(TreeNode root) {
+        return validbst(root).ans;
+    }
+     public BstPair validbst(TreeNode root){
+        if(root == null){
+            return new BstPair();
+        }
+        BstPair lbstp= validbst(root.left);
+        BstPair rbstp= validbst(root.right);
+        BstPair sbstp= new BstPair();
+        sbstp.min =Math.min(root.val, Math.min(lbstp.min, rbstp.min));
+        sbstp.max= Math.max(root.val, Math.max(lbstp.max, rbstp.max));
         
+        sbstp.isbst= lbstp.isbst && rbstp.isbst && root.val >lbstp.max && root.val <rbstp.min;
+         sbstp.sum =lbstp.sum +rbstp.sum + root.val;
+        if(sbstp.isbst){
+            sbstp.ans =Math.max(sbstp.sum,Math.max(lbstp.ans, rbstp.ans));
+            
+        }else{
+            sbstp.ans = Math.max(lbstp.ans, rbstp.ans);
+        }
+        return sbstp;
+    }
+     class BstPair{
+        boolean isbst = true;
+        long min= Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+        int sum=0;//sare nodes ka sum
+        int ans=0; //bst ka sum 
     }
 }
 }
+
