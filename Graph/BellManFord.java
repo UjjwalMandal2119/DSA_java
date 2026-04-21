765b22c2a7a20f2a4cd72e0dc72733efcf1752c8
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-04-21 20:13:28 +0530

    Graph

---
+++ Graph/BellManFord.java
@@ -0,0 +1,71 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class BellManFord {
+   HashMap<Integer, HashMap<Integer, Integer>> map;
+   
+   public BellManFord(int v) {
+    map = new HashMap<>();
+    for(int i=1; i<=v; i++){
+        map.put(i, new HashMap<>());
+    }
+   }
+
+   public void AddEage(int v1, int v2, int cost){
+    map.get(v1).put(v2, cost);
+   }
+
+   public void BellManFordAlgo(int src){
+    int v = map.size();
+    int [] dis = new int[v + 1];
+    for(int i=1; i<dis.length; i++){
+        dis[i]= 10000_000;
+    }
+    dis[src] = 0;
+    List<EdgePair> ll = AllEdge();
+    for(int i=1; i<=v-1; i++){
+        for(EdgePair e : ll){
+            if(dis[e.e2] > dis[e.e1] + e.cost){
+                dis[e.e2] = dis[e.e1] + e.cost;
+            }
+        }
+    }
+    for(int i=1; i<dis.length; i++){
+        System.out.println(dis[i] + " ");
+    }
+   }
+
+   public class EdgePair{
+    int e1;
+    int e2;
+    int cost;
+
+    public EdgePair(int e1, int e2, int cost){
+        this.e1 = e1;
+        this.e2= e2;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString(){
+        return this.e1 + "-->" + this.e2 + " @ " + this.cost;
+    }
+   }
+
+   public List<EdgePair> AllEdge(){
+    List<EdgePair> ll = new ArrayList<>();
+    for(int e1 : map.get(e1).keySet()){
+        int cost = map.get(e1).get(e2);
+        ll.add(new EdgePair(e1, e2, cost));
+    }
+   }
+   return ll;
+}
+
+public static void main(String [] args){
+   
+}
+}
