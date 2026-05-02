5f2da9ed3fc7c8cd42289c74d0c44a94b163817a
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-05-03 01:37:46 +0530

    learning

--- DP/Fibo.java
+++ DP/Fibo.java
@@ -6,6 +6,10 @@ public class Fibo {
     public static void main(String args[]){
         Scanner sc=  new Scanner(System.in);
         int n=sc.nextInt();
+        
+        int [] dp = new int[n+1];
+        
+        System.out.println(fibTD(n, dp));
         System.out.println(fib(n));
     }
 
@@ -21,6 +25,8 @@ public class Fibo {
     //with the concept of Dynamic Programming
 
     //Time Complexity O(n)
+
+    //Top - Down approach - Memoziation
     public static int fibTD(int n, int[] dp){
         if(n==0 ||n==1){
             return n;
@@ -29,8 +35,20 @@ public class Fibo {
             return dp[n];
         }
 
-        int f1 = fibTD(n-1, dp);
-        int f2 = fibTD(n-2, dp);
-        return dp[n]= f1 +f2;
+        int f1 = fibTD(n-1, dp); //dp[n-1]
+        int f2 = fibTD(n-2, dp); //dp[n-2]
+        return dp[n]= f1 +f2;    //dp[n] =dp[n-1] + dp[n-2]
+    }
+
+    //Bottom-up approach -Tabulation
+    
+    public static int fibBU(int n){
+        int [] dp = new int [n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<dp.length; i++){
+            dp[i] = dp[i-1] + dp[i -2];
+        }
+        return dp[n];
     }
 }
