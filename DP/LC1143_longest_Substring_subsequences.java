9823795a443748fb2e9d7601f730f759e7a24d36
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-05-10 23:25:14 +0530

    learning

--- DP/LC1143_longest_Substring_subsequences.java
+++ DP/LC1143_longest_Substring_subsequences.java
@@ -1,6 +1,6 @@
 package DP;
 
-import java.lang.reflect.Array;
+import java.util.*;
 
 public class LC1143_longest_Substring_subsequences {
     public static void main(String[] args) {
@@ -19,6 +19,20 @@ public class LC1143_longest_Substring_subsequences {
         System.out.println(lcs(s1,  s2, 0, 0, dp));
     }
     public static int lcs(String s1, String s2, int i, int j, int [][] dp){
-        
+        if(i==s1.length() || j == s2.length()){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int ans=0;
+        if(s1.charAt(i) == s2.charAt(j)){
+            ans=1+lcs(s1, s2, i+1, j+1, dp);
+        }else{
+            int a = lcs(s1, s2, i+1, j, dp);
+            int b= lcs(s1, s2, i, j+1, dp);
+            ans = Math.max(a,b);
+        }
+        return dp[i][j]=ans;
     }
 }
