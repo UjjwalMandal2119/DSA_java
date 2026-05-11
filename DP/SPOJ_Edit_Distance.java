645ca4193acd9f1f7b60249d74e58051521a281f
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-05-11 07:01:29 +0530

    learning

--- DP/SPOJ_Edit_Distance.java
+++ DP/SPOJ_Edit_Distance.java
@@ -47,6 +47,9 @@ public class SPOJ_Edit_Distance {
             Arrays.fill(a, -1);
         }
         System.out.println(EditDistanceDP(s, t, 0, 0, dp));
+
+        //bottom up approach DP
+        System.out.println(EditDistanceBU(s, t));
         
     }
     //Recursion
@@ -91,5 +94,33 @@ public class SPOJ_Edit_Distance {
             ans=Math.min(I, Math.min(D, R))+1;
         }
       return dp[i][j]=ans;
+    }
+
+    //DP bottom up approach
+    public static int EditDistanceBU(String s, String t){
+        int [][] dp= new int[s.length()+1][t.length()+1];
+        for(int i=1; i<dp[0].length; i++){
+            dp[0][i]=i;
+
+        }
+         for(int i=1; i<dp.length; i++){
+            dp[i][0]=i;
+            
+        }
+        for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                int ans=0;
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    ans = dp[i-1][j-1];
+                }else{
+                    int D= dp[i-1][j];
+                    int I= dp[i][j-1];
+                    int R=dp[i-1][j-1];
+                    ans = Math.min(I, Math.min(D,R))+1;
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
