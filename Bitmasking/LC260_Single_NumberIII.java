9b1a9bd9d6e00f5f6a20805f957a8e879267b2d2
Ujjwal Mandal <ujjwal815351@gmail.com>
2026-05-13 05:15:28 +0530

    learning

--- Bitmasking/LC260_Single_NumberIII.java
+++ Bitmasking/LC260_Single_NumberIII.java
@@ -2,6 +2,27 @@ package Bitmasking;
 
 public class LC260_Single_NumberIII {
     public static void main(String[] args) {
-        
+        int [] arr ={1,2,1,3,2,5};
+        int [] res=SingleNumber(arr);
+        for(int n:res){
+            System.out.println(n);
+        }
+
+    }
+    public static int [] SingleNumber(int [] arr){
+        int ans=0;
+        for(int i=0; i<arr.length; i++){
+            ans ^=arr[i];
+        }
+        int mask=(ans &(~(ans -1)));
+        int a=0;
+        for(int i=0; i<arr.length; i++){
+            if((arr[i] & mask) !=0){
+                a^= arr[i];
+            }
+        }
+        int b=ans^a;
+
+        return new int[]{a, b};
     }
 }
